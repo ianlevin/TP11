1c457723c9949eade560fef2cd93a60e7a667dd8
IAN GABRIEL LEVIN <47295402@ort>
2023-11-02 09:05:19 -0300

    controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,14 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Filtro(string filtro)
+    {
+        switch(filtro){
+            case "color":
+                break;
+        }
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
