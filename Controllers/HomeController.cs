273bb2723f93a39f197bc5582e609a7732f7cd34
IAN GABRIEL LEVIN <47295402@ort>
2023-11-07 11:46:59 -0300

    funcion login controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,11 +27,11 @@ public class HomeController : Controller
     {
         Usuario User = BD.ObtenerUsuario(Username);
         ViewBag.Usuario = User;
-        if(BD.ObtenerUsuario(Username) == null || Usuario.GetContrasena() != Contrasena){
+        if(BD.ObtenerUsuario(Username) == null || User.GetContrasena() != Contrasena){
             ViewBag.Error = "Usuario o contraseña incorrectos";
-            return View("Login");
+            return View("Index");
         }
-        return View();
+        return View("Home");
         
     }
     public IActionResult SignUp()
