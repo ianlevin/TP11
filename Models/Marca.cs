471ad8f02dd08e8f6c47bd46acbae360906f813d
LUCAS NICOLAS KOZIUPA <47572827@ort>
2023-11-21 11:55:34 -0300

    filtro marcas

--- Models/Marca.cs
+++ Models/Marca.cs
@@ -1,6 +1,6 @@
 public class Marca{
     public int IdMarca {get; set;}
-    public string Nombre {get; set;}
+    public string NombreMarca {get; set;}
     public string Pais {get; set;}
     public string Direccion {get; set;}
     public string SitioWeb {get; set;}
@@ -8,7 +8,7 @@ public class Marca{
     public Marca(){}
     public Marca(int idM, string nom, string pa, string dire, string web){
         IdMarca = idM;
-        Nombre = nom;
+        NombreMarca = nom;
         Pais = pa;
         Direccion = dire;
         SitioWeb = web;
