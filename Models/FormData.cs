a96755b91a55c167e0eb7ab9b2c396b418ed1471
Tomas <czernuszkatomas@gmail.com>
2024-04-25 23:06:02 -0300

    Volver en el formulario, eliminacion de uso excesivo de tempdata

---
+++ Models/FormData.cs
@@ -0,0 +1,19 @@
+public class FormData
+{
+    public int PasoActual { get; set; }
+    public int Kilometraje { get; set; }
+    public string Matricula { get; set; }
+    public string Motor { get; set; }
+    public int IdMarca { get; set; }
+    public int Ano { get; set; }
+    public int Asientos { get; set; }
+    public int IdColor { get; set; }
+    public int IdModelo { get; set; }
+    public bool AireAcondicionado { get; set; }
+    public bool ABS { get; set; }
+    public bool Airbag { get; set; }
+    public int IdTransmision { get; set; }
+    public int IdDireccion { get; set; }
+    public int Precio { get; set; }
+    public int IdUsuario { get; set; }
+}
